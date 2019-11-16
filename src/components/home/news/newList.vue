756257ff97ef1d13d39b6be83def34a6365735f1
陈yin <2672324727@qq.com>
2019-11-16 16:48:21 +0800

    newlist

--- src/components/home/news/newList.vue
+++ src/components/home/news/newList.vue
@@ -1,15 +1,67 @@
 <template>
-    <div>
-      newList
+  <div>
+    <van-list v-model="loading" :finished="finished" direction @load="onLoad" finished-text="没有更多了">
+      <van-cell>
+
+      </van-cell>
+    </van-list>
+    <div v-for="item in list" :key="item.id" class="image">
+      <!-- 左 图片 -->
+      <div>
+        <img :src="item.img_url" alt="">
+      </div>
+      <!-- 右 -->
+      <div>
+
+      </div>
     </div>
+    
+  </div>
+
 </template>
 
 <script>
 export default {
-  name: 'newList'
+  name: 'newList',
+  data() {
+    return {
+      list: [],
+      loading: false,
+      finished: false,
+      leng: 0,
+      direction: "up"
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    async getList() {
+      const { data: res } = await this.$http.get('/api/getnewslist')
+      console.log(res.message);
+      this.list = res.message
+      this.leng = res.message.length
+    },
+    onLoad() {
+      setTimeout(() => {
+        console.log(this.leng);
+        if (this.leng >= 10)
+          // 加载状态结束
+          this.loading = false;
+
+        // 数据全部加载完成
+        if (this.list.length >= 10) {
+          this.finished = true;
+        }
+      }, 500);
+    }
+
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+.image {
+  height: 68px;
+}
 </style>
