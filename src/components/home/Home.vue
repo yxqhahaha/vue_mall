28ef9f4503e4ddfd80ff066d1b7fffbb371e747d
cnyangx <yangxiuqian95@outlook.com>
2019-11-16 14:39:06 +0800

    init

---
+++ src/components/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>welcome</h3>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
