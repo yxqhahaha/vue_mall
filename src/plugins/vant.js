67ad81a53694386119213d32a89e2550a99d4e36
Aurore <284873865@qq.com>
2019-11-17 23:53:52 +0800

    !3 devMergeMaster * !2 member+search+addgoods * addgoods-search-member * !1 重定向主页 * 分支

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
-import { Button } from 'vant'
+import { Button, Search, Divider, Icon, Cell, CellGroup, Tag, SwipeCell, Notify, Dialog, Loading, Field } from 'vant'
 import 'vant/lib/index.css'
 
 Vue.use(Button)
+Vue.use(Search)
+Vue.use(Divider)
+Vue.use(Icon)
+Vue.use(Cell).use(CellGroup)
+Vue.use(Tag)
+Vue.use(SwipeCell)
+Vue.use(Notify)
+Vue.use(Dialog)
+Vue.use(Loading)
+Vue.use(Field)
