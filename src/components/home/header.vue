6adff7d4b5fe1a7ad7fe1de0ecd8b3941f0bd631
cnyangx <yangxiuqian95@outlook.com>
2019-11-18 16:41:47 +0800

    laoyang

--- src/components/home/header.vue
+++ src/components/home/header.vue
@@ -1,49 +1,38 @@
 <template>
-    
-       <div class="title">
-            <i @click="back">返回</i>
-            <span>
-              黑马程序员.vant
-            </span>
-          </div>
-    
+   <div class="title">
+        <i @click="back">返回</i>
+        <span>
+          黑马程序员.vant
+        </span>
+      </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-      nowURL:''
-
-        }
-    },
-    methods: {
-      back(){
-        this.$router.go(-1)
-      }
-        
-    },
-    created() {
-          this.nowURL=window.location.hash
-          console.log(this.nowURL);
-          
-
-          
-    },
-    updated() {
-          this.nowURL=window.location.hash
-          console.log(this.nowURL);
-          
-
-          
-    },
-
-    computed: {
-        
-    },
-    component: {
-
+  data () {
+    return {
+      nowURL: ''
     }
+  },
+  methods: {
+    back () {
+      this.$router.go(-1)
+    }
+  },
+  created () {
+    this.nowURL = window.location.hash
+    console.log(this.nowURL)
+  },
+  updated () {
+    this.nowURL = window.location.hash
+    console.log(this.nowURL)
+  },
+
+  computed: {
+  },
+  component: {
+
+  }
 }
 </script>
 
@@ -61,9 +50,8 @@ export default {
   z-index: 999;
 }
 .title i {
- 
   position: absolute;
   left: 20px;
   font-style: normal;
-} 
+}
 </style>
