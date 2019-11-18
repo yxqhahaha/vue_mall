e7de2cd3d530d802842657acd836af94f0713f7f
谭铁灯 <1486030616@qq.com>
2019-11-18 14:18:31 +0800

    购物车完成

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'vant'
+import { Row, Col, Card, Button, Tag, Stepper, SubmitBar, Image } from 'vant'
 import 'vant/lib/index.css'
 
-Vue.use(Button)
+Vue.use(SubmitBar);
+Vue.use(Button);
+Vue.use(Stepper);
+Vue.use(Tag);
+Vue.use(Image);
+Vue.use(Card);
+Vue.use(Row).use(Col)
